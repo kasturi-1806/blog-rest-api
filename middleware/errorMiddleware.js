08ee4600d00b190685ab37323f9c07613c6a5300
Kasturi Sambarekar <kasturi.sambarekar@gmail.com>
2026-09-19 14:46:19 +0530

    Clean up errorMiddleware.js formatting
    Remove unnecessary blank lines in error middleware.

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,9 +1,7 @@
 function errorMiddleware(err, req, res, next) {
   console.error(err.message);
-
   res.status(500).json({
     message: "Internal server error"
   });
 }
-
 module.exports = errorMiddleware;
